ffad1fe9350b1e92b5bf688e3e6ed95b7fe63534
madandan00 <1910763812@qq.com>
2017-01-03 15:17:04 +0800

    03

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,24 +1,5 @@
-require('../styles/usage/app.scss');
-//
-//console.log(2)
-//var str = require("./common.js");
-//console.log(str)
-//var date = require('./date.js');
-//console.log(date.getTime())
-
-//import name from './date';
-//console.log(name)
-
-//var ajax = require('./child');
-//ajax.getPerson();
-//
-//
-//import ES6 from './es6';
-
-var common = require('./utils/util.common.js');
-
-//common.render("hahha");
-
-var html = require('./tpls/home.html');
-//console.log(html);
+require('./styles/usage/app.scss');
+var common = require('./scripts/utils/util.common.js');
+var html = require('./scripts/tpls/list.html');
 common.render(html);
+require('./scripts/views/list.js')
